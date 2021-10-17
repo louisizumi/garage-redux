14063303066fba0ad19ce082dcb5de7d8e6de6e6
Louis Drinkwater <izumilouis95@gmail.com>
2021-10-17 23:21:59 +0100

    Add form validations

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -5,12 +5,32 @@ import { Link } from 'react-router-dom';
 
 import { createCar } from '../actions';
 
+const required = (value) => {
+  return value ? undefined : '* Required';
+};
+
+const validPlate = (value) => {
+  return !/[A-Z0-9]+/.test(value) ? 'Plate number must be allcaps and contain only the characters A-Z 0-9' : undefined;
+};
+
 class CarsNew extends React.Component {
   onSubmit = (values) => {
     this.props.createCar(this.props.garage, values, () => {
       this.props.history.push('/'); // Navigate after submit
     });
   }
+
+  renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div className="form-group">
+      <label htmlFor={input}>{label}</label>
+      <div className="form-control">
+        <input {...input} type={type} placeholder={label} />
+        <p className="form-validation">
+          {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </p>
+      </div>
+    </div>
+  )
 
   render() {
     return (
@@ -23,46 +43,38 @@ class CarsNew extends React.Component {
         </header>
         <main className="garage__main">
           <form className="form-car" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <div className="form-group">
-              <label htmlFor="brand">Brand</label>
-              <Field
-                name="brand"
-                type="text"
-                component="input"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="brand">Model</label>
-              <Field
-                name="model"
-                type="text"
-                component="input"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="brand">Owner</label>
-              <Field
-                name="owner"
-                type="text"
-                component="input"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="brand">Plate</label>
-              <Field
-                name="plate"
-                type="text"
-                component="input"
-                className="form-control"
-              />
-            </div>
+            <Field
+              name="brand"
+              label="Brand"
+              type="text"
+              component={this.renderField}
+              validate={required}
+            />
+            <Field
+              name="model"
+              label="Model"
+              type="text"
+              component={this.renderField}
+              validate={required}
+            />
+            <Field
+              name="owner"
+              label="Owner"
+              type="text"
+              component={this.renderField}
+              validate={required}
+            />
+            <Field
+              name="plate"
+              label="Plate"
+              type="text"
+              component={this.renderField}
+              validate={[required, validPlate]}
+            />
             <button
               className="btn btn--submit"
               type="submit"
-              disabled={this.props.pristine || this.props.submitting}
+              disabled={this.props.submitting}
             >
               Add Car
             </button>
